d3dda33bf6d4828dddb25cbd157b66ed297764c7
= <pmosadeqzadeh@gmail.com>
2025-04-16 20:05:52 +0330

    chore: create simple controller routes

--- src/blogs/blog.controller.ts
+++ src/blogs/blog.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
-@Controller('/blogs')
-export class BlogController {}
+@Controller('/api/v1/blogs')
+export class BlogController {
+  @Get()
+  getAllBlogs() {
+    return 'all blogs';
+  }
+
+  @Get('/:id')
+  getSingleBlog(@Param('id') id: string) {
+    return `single blog ${id}`;
+  }
+
+  @Get('/category/:category')
+  getCategoryBlog(@Param('category') category: string) {
+    return `category ${category}`;
+  }
+
+  @Post('/create/:id')
+  // * Change the body type (dto)
+  createSingleBlog(@Body() body: Record<string, string>) {
+    return `create blog ${body.name}`;
+  }
+}
