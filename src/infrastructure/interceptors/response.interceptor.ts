1a8f9b269e9bb841672cad6f55744143bcb2530d
= <pmosadeqzadeh@gmail.com>
2025-04-17 01:25:28 +0330

    chore: interceptor for res handling

---
+++ src/infrastructure/interceptors/response.interceptor.ts
@@ -0,0 +1,43 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { CustomError } from '../interfaces';
+
+@Injectable()
+export class ResponseInterceptor<T>
+  implements NestInterceptor<T, Record<string, unknown>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Record<string, unknown>> {
+    return next.handle().pipe(
+      map((data: Record<string, unknown> | Record<string, unknown>[]) => ({
+        status: 'success',
+        message: 'Request successful',
+        data,
+      })),
+      catchError((err: CustomError) => {
+        console.log(err, 'err');
+        return throwError(
+          () =>
+            new HttpException(
+              {
+                status: 'error',
+                message: err.message || 'Something went wrong',
+                statusCode: err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+              },
+              err.status || HttpStatus.INTERNAL_SERVER_ERROR,
+            ),
+        );
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from './infrastructure';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
