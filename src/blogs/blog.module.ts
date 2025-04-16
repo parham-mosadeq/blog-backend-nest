1b7ef0146619ae79bf17ba6913e788fd40ad3cc7
= <pmosadeqzadeh@gmail.com>
2025-04-17 00:02:25 +0330

    chore: added entity to blog module

--- src/blogs/blog.module.ts
+++ src/blogs/blog.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BlogController } from './blog.controller';
 import { BlogService } from './blog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogEntity } from './entity';
 
 @Module({
   controllers: [BlogController],
   providers: [BlogService],
+  imports: [TypeOrmModule.forFeature([BlogEntity])],
   exports: [BlogModule],
 })
 export class BlogModule {}
