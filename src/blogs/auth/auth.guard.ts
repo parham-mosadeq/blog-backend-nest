d178b4e93d0ed620b064453597053f7ab1a4c1bd
= <pmosadeqzadeh@gmail.com>
2025-04-17 18:43:45 +0330

    chore: added auth guard

---
+++ src/blogs/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Missing authorization header');
+    }
+
+    const [type, token] = authHeader.split(' ');
+
+    if (type !== 'Bearer' || token !== 'valid_token') {
+      throw new UnauthorizedException('Invalid or missing token');
+    }
+
+    return true;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from './infrastructure';
+import { AuthGuard } from './blogs/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalGuards(app.get(AuthGuard));
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
