eb2006ee76aa5e51499ad6492f90186b5ed6ad2d
= <pmosadeqzadeh@gmail.com>
2025-04-20 21:59:11 +0330

    chore: added jwt

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,6 +5,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -15,13 +16,22 @@ export class AuthGuard implements CanActivate {
     console.log(authHeader, 'authHeader');
 
     if (!authHeader) {
-      throw new UnauthorizedException('Missing authorization header');
+      throw new UnauthorizedException('Token is not valid');
     }
 
     const [type, token] = authHeader.split(' ');
 
     if (type !== 'Bearer' || token !== 'valid_token') {
       throw new UnauthorizedException('Invalid or missing token');
+    }
+
+    try {
+      const secret = process.env.JWT_SECRET || 'default_secret';
+      const decoded = jwt.verify(token, secret);
+
+      request['user'] = decoded;
+    } catch {
+      throw new UnauthorizedException('Invalid or expired token');
     }
 
     return true;
