012e254ebcd7bceed2ac03c5830887e7ea5a4ed6
= <pmosadeqzadeh@gmail.com>
2025-04-20 21:36:29 +0330

    fix: linter errors

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import { AuthUserDto } from './dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller({
   path: '/api/v1/auth',
   version: '1',
 })
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() body: AuthUserDto) {
+    const token = await this.authService.validateUser(body);
+    if (!token) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthEntity } from './entity';
+import { AuthUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    @InjectRepository(AuthEntity)
+    private readonly authRepo: Repository<AuthEntity>,
+  ) {}
+
+  async validateUser(
+    dto: AuthUserDto,
+  ): Promise<{ access_token: string } | null> {
+    const user = await this.authRepo.findOne({
+      where: { username: dto.username },
+    });
+
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(dto.password, user.password);
+
+    if (!isMatch) return null;
+
+    const payload = { sub: user.id, username: user.username };
+    const access_token = this.jwtService.sign(payload);
+
+    return { access_token };
+  }
+}
